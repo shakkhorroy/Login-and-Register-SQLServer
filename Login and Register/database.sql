b8e509641cc1f9e38abde58e69235d6eda04d400
asus <asus@LAPTOP-ELKKPBMN>
2026-09-06 03:48:28 +0600

    Update database.sql

--- Login and Register/database.sql
+++ Login and Register/database.sql
@@ -1,3 +1,16 @@
-USE db_users;
+CREATE DATABASE db_users;
+GO
 
-SELECT * FROM tbl_users;
+USE db_users;
+GO
+
+CREATE TABLE tbl_users (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    username NVARCHAR(50) NOT NULL UNIQUE,
+    password NVARCHAR(100) NOT NULL
+);
+GO
+
+INSERT INTO tbl_users (username, password)
+VALUES ('admin', 'admin123');
+GO
